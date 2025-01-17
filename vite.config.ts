05f57ff36ab61f63fc6835c75b6c2d713b687362
Mauricio Giaconia <mauriciogiaco@gmail.com>
2025-01-17 01:46:42 -0300

    chore: vite server config

--- vite.config.ts
+++ vite.config.ts
@@ -78,17 +78,6 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    host: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": resolve(root),
